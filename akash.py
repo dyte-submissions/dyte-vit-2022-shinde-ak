4c3bb3492c26afea670b71aee92d3851ec1abd9c
shinde-ak <shindeakashvishwasrao@gmail.com>
2022-05-31 18:12:17 +0530

    be modified ..no electricity laptop shutdown

--- akash.py
+++ akash.py
@@ -8,9 +8,7 @@ import version
 
 app = typer.Typer()
 
-@app.command()
-def hello():
-    print("hello added...")
+
 
 @app.command()
 def check_version(a:str,b:str):
@@ -18,16 +16,23 @@ def check_version(a:str,b:str):
     data =pd.read_csv(a)
     link= data["repo"]
     for i in range(link.shape[0]):
-        fetched = version.get_version(link[i])
+        modified = modify(link[i])
+        fetched = version.get_version(modified)
         data["version"] = fetched
         data["version_satisfied"] = "true" if validate(split_version,fetched) else "false" 
+    pd.to_csv("output.csv")
+    print("sucessfully fetched and data stored in output.csv file...")
+    
 
 
 @app.command()
 def add_version():
     print("adding new version...")
 
-
+def modify(a:str):
+    a.replace("https://github.com","https://raw.githubusercontent.com")
+    a +="/main/package-lock.jsonn"
+    return a
 
 def validate(a:list,b:str):
     fetched = b.split('.')
@@ -44,5 +49,8 @@ def validate(a:list,b:str):
                     return True
                 return False
     return True
+
+
+
 if __name__ == "__main__":
     app()
--- version.py
+++ version.py
@@ -6,8 +6,10 @@ import requests
 
 def get_version(link:str):
     response = requests.get(link)
+    print("responce from server is :")
     print(response.status_code)
     data = response.json()
+    print("version fetched is :",data['version'] )
     return (data['version'])
 
 if __name__=="__main__":
